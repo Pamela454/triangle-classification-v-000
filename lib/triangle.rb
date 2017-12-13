051439b4ae7ca3acda17fb81a081409d043d8825
Pamela Frieze <pfrieze@gmail.com>
2017-12-13 05:32:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,6 +18,11 @@ class Triangle
   end
 end
 
+def evaluate_triangle
+    raise TriangleError if kind != :isosceles, :equilateral, :scalene
+  end
+end
+
 class TriangleError < StandardError
 end
 end
