426e1b9dcb5149fa92c4ff90dab8047a3c4fb02e
Pamela Frieze <pfrieze@gmail.com>
2017-12-13 05:02:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,12 @@
 class Triangle(long, longer, longest)
   # write code here
   def initialize(type)
-    @type = type 
+    @type = type
   end
 
   def kind
   end
+end
+
+class TriangleError < StandardError
 end
